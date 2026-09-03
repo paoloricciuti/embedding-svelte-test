1173303ee15d5f163e2ea7cbf790febb7992b39f
paoloricciuti <ricciutipaolo@gmail.com>
2026-09-03 18:13:43 +0200

    chore: import url

--- src/routes/test/+server.ts
+++ src/routes/test/+server.ts
@@ -1,16 +1,19 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import url from '#lib/index.ts?url';
 
 export async function GET() {
 	try {
 		const file = await fs.readFile(path.join(process.cwd(), 'file.txt'), 'utf-8');
 		return Response.json({
 			file,
+			url,
 			cwd: process.cwd()
 		});
 	} catch (e) {
 		return Response.json({
 			error: e instanceof Error ? e.message : String(e),
+			url,
 			cwd: process.cwd()
 		});
 	}
